fa3b5a369d3830e4e9d5a9a03ffdb9cb856b0fdd
Jason Ngan <jason.ngan@shopee.com>
2022-05-03 04:20:35 +0800

    add drag and drop

--- image-uploader/frontend/src/components/Dashboard/Dashboard.js
+++ image-uploader/frontend/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Upload from "../Upload/Upload";
 import Uploading from "../Uploading/Uploading";
 import Uploaded from "../Uploaded/Uploaded";
 
 function Dashboard() {
+  const [file, setFile] = useState(null)
+  const [submitted, setSubmitted] = useState(false)
+
+  const onFileSubmit = (file) => {
+    setFile(file);
+
+    setInterval(() => {
+      setFile(null)
+      setSubmitted(true)
+    }, 500)
+  };
+
+  const getComponent= () => {
+    if (file === null && !submitted) {
+      return <Upload onFileSubmit={onFileSubmit} />;
+    } else if (submitted) {
+      return <Uploaded/>
+    }
+
+    return <Uploading/>
+  }
+
   return <div className="dashboard">
-    <Uploaded/>
+  {
+    getComponent()  
+  }
   </div>;
 }
 
