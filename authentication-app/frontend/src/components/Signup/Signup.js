acb8bed0b53f5ee67a5110df9f7d46cbfda2449c
Jason Ngan <jason.ngan@shopee.com>
2022-05-07 10:46:03 +0800

    add signup-form

--- authentication-app/frontend/src/components/Signup/Signup.js
+++ authentication-app/frontend/src/components/Signup/Signup.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Text from "../Text/Text"
 import Button from "../Button/Button"
 
-import devLogo from "../../assets/devchallenges.svg"
+import lightThemeDevLogo from "../../assets/devchallenges.svg"
+import darkThemeDevLogo from "../../assets/devchallenges-light.svg";
 import githubLogo from "../../assets/Github.svg"
 import twitterLogo from "../../assets/Twitter.svg";
 import googleLogo from "../../assets/Google.svg";
 import facebookLogo from "../../assets/Facebook.svg";
 
-function Signup() {
+function Signup({isSignup, isDarkTheme}) {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
@@ -26,15 +27,28 @@ function Signup() {
 
   return (
     <div className="signup">
-      <img className="signup__logo" src={devLogo} />
-      <Text size="1.1rem" bd="600" mgTop="1">
-        Join thousands of learners from around the world
-      </Text>
+      <img
+        className="signup__logo"
+        src={isDarkTheme ? darkThemeDevLogo : lightThemeDevLogo}
+      />
+      {isSignup ? (
+        <>
+          <Text size="1.1rem" bd="600" mgTop="1">
+            Join thousands of learners from around the world
+          </Text>
 
-      <Text size="0.9rem" bd="400" mgTop="1">
-        Master web development by making real-life projects. There are multiple
-        paths for you to choose.
-      </Text>
+          <Text size="0.9rem" bd="400" mgTop="1">
+            Master web development by making real-life projects. There are
+            multiple paths for you to choose.
+          </Text>
+        </>
+      ) : (
+        <>
+          <Text size="1.1rem" bd="600" mgTop="1">
+            Login
+          </Text>
+        </>
+      )}
 
       <input
         type="text"
@@ -54,11 +68,11 @@ function Signup() {
         width="100%"
         height="2.5rem"
         border="8px"
-        mgTop="1"
+        mgTop="1.5"
         mgBtm="1.5"
         onClick={onSubmit}
       >
-        Start coding now
+        {isSignup ? "Start coding now" : "Login"}
       </Button>
       <Text bd="400" size="0.9rem" color="secondary" align="center">
         or continue with these social profile
@@ -72,7 +86,16 @@ function Signup() {
       </div>
 
       <Text bd="400" size="0.9rem" align="center" color="secondary">
-        Already a member? <a>Login</a>
+        {isSignup ? (
+          <>
+            Already a member? <a href="http://localhost:3001/login">Login</a>
+          </>
+        ) : (
+          <>
+            Don't have an account yet?{" "}
+            <a href="http://localhost:3001/signup">Register</a>
+          </>
+        )}
       </Text>
     </div>
   );
